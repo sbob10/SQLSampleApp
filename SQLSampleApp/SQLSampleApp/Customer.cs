2b506c70835106a3a45542b9abef760ae9c5681b
David Golovin <david.golovin19@gmail.com>
2018-04-20 16:50:00 +0200

    Finalized first MVVM logic

---
+++ SQLSampleApp/SQLSampleApp/Customer.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLSampleApp
+{
+    class Customer : BindableBase
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "EMail: " + Mail + "Telefonnummer: " + PhoneNumber;
+        }
+    }
+
+}
---
+++ SQLSampleApp/SQLSampleApp/MainWindowViewModel.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SQLSampleApp
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        #region Properties
+
+        private Customer _addTabCustomer;
+
+        public Customer AddTabCustomer
+        {
+            get { return _addTabCustomer; }
+            set { SetProperty(ref _addTabCustomer, value, () => AddTabCustomer); }
+        }
+
+        #endregion Properties
+
+        #region Data
+
+
+
+        #endregion Data
+
+        #region Commands&Services
+
+        public ICommand AddCustomerCommand { get; set; }
+
+        public IMessageBoxService MessageBoxService { get { return ServiceContainer.GetService<IMessageBoxService>(); } set { } }
+        public IServiceContainer IServiceContainer { get; set; }
+
+        #endregion Commands&Services
+
+        #region Initialisations
+
+        public MainWindowViewModel()
+        {
+            InitCommandsAndServices();
+            InitValuesAndCollections();
+        }
+
+        private void InitValuesAndCollections()
+        {
+            AddTabCustomer = new Customer();
+        }
+
+        private void InitCommandsAndServices()
+        {
+            IServiceContainer = new ServiceContainer(this);
+            ServiceContainer.RegisterService(new DevExpress.Mvvm.UI.MessageBoxService());
+
+            AddCustomerCommand = new DelegateCommand(AddCustomer);
+        }
+
+        #endregion Initialisations
+
+        #region Public Methods
+
+
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+
+
+        #endregion Private Methods
+
+        #region Command Methods
+
+        private void AddCustomer()
+        {
+            //Here the database stuff...
+            MessageBoxService.ShowMessage("Benutzer hinzugefügt: " + AddTabCustomer.ToString(), "Message", MessageButton.OK, MessageIcon.Information);
+        }
+
+        #endregion Command Methods
+
+    }
+}
